0d09bb1f9fc4cf2a7c5cb3f918a6bb6961b32e18
Yakup <yakupjoraev@gmail.com>
2023-06-16 17:23:03 +0300

    added initial files

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+// Custom scripts
+
+
+function sidebarFunctions() {
+  let menuItems = document.querySelectorAll('[data-menu-item]');
+
+  menuItems.forEach(menuItem => {
+    const arrow = menuItem.querySelector('[data-menu-item-arrow]');
+    if (arrow) {
+      arrow.addEventListener('click', () => {
+        menuItem.classList.toggle('active');
+      });
+    }
+  });
+}
+
+sidebarFunctions();
+
