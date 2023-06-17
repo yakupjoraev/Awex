b3a4071108a0412ca3e43c4133e76c38bfe3aadb
Yakup <yakupjoraev@gmail.com>
2023-06-18 02:06:26 +0300

    added new blocks

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,40 @@
 
 
 function sidebarFunctions() {
+  const container = document.querySelector('.sidebar');
+
+  if (!container) {
+    return null
+  }
+
   let menuItems = document.querySelectorAll('[data-menu-item]');
 
   menuItems.forEach(menuItem => {
     const arrow = menuItem.querySelector('[data-menu-item-arrow]');
     if (arrow) {
       arrow.addEventListener('click', () => {
-        menuItem.classList.toggle('active');
+        menuItem.classList.toggle('show');
       });
     }
   });
+
+  let sideBarItems = document.querySelectorAll('.sidebar__menu-link, .sidebar__menu-sublink')
+
+  sideBarItems.forEach(sideBarItem => {
+    sideBarItem.addEventListener('click', (event) => {
+      // Проверяем, что клик не произошел на элементе с классом .sidebar__menu-arrow
+      if (!event.target.classList.contains('sidebar__menu-arrow')) {
+        // Удаляем класс active у всех элементов
+        sideBarItems.forEach(item => {
+          item.classList.remove('active');
+        });
+
+        // Добавляем класс active к выбранному элементу
+        sideBarItem.classList.add('active');
+      }
+    });
+  });
+
 }
 
 sidebarFunctions();
