d84fe0abe3f717e4d97d50f1f431014d1b002d02
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-08-04 20:32:37 +0300

    feat: unfold tabs on settings page

--- front-react/src/app/pages/SettingsPage/SettingsPageLayout/index.tsx
+++ front-react/src/app/pages/SettingsPage/SettingsPageLayout/index.tsx
@@ -1,8 +1,37 @@
-import React from "react";
-import { useState } from "react";
+import classNames from "classnames";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
 export function SettingsPageLayout() {
+  const tabsRef = useRef<HTMLDivElement>(null);
+  const [tabsOpened, setTabsOpened] = useState(false);
+
+  useEffect(() => {
+    if (!tabsOpened) {
+      return;
+    }
+
+    const handleDocumentClick = (ev: MouseEvent) => {
+      if (
+        tabsRef.current &&
+        ev.target instanceof Element &&
+        !tabsRef.current.contains(ev.target)
+      ) {
+        setTabsOpened(false);
+      }
+    };
+
+    document.addEventListener("click", handleDocumentClick);
+
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, [tabsOpened]);
+
+  const handleTabsArrowClick = (_ev: React.MouseEvent<HTMLImageElement>) => {
+    setTabsOpened(!tabsOpened);
+  };
+
   return (
     <section className="settings">
       <div className="deposits__header">
@@ -10,7 +39,11 @@ export function SettingsPageLayout() {
       </div>
 
       <div className="settings__inner">
-        <div className="settings__list" data-payment-details-content>
+        <div
+          className={classNames("settings__list", { show: tabsOpened })}
+          data-payment-details-content
+          ref={tabsRef}
+        >
           <NavLink className="settings__item" to="/settings" end>
             Профиль
           </NavLink>
@@ -20,6 +53,7 @@ export function SettingsPageLayout() {
             src="/img/icons/arrow-down-white.svg"
             alt=""
             data-payment-details-btn
+            onClick={handleTabsArrowClick}
           />
 
           <NavLink className="settings__item" to="/settings/requisites">
