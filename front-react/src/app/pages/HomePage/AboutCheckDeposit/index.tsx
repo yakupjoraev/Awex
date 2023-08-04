d64ad26222ddec5863d0b3fde44ddfaed8e68479
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-08-04 19:26:44 +0300

    feat: projects selector

--- front-react/src/app/pages/HomePage/AboutCheckDeposit/index.tsx
+++ front-react/src/app/pages/HomePage/AboutCheckDeposit/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import classNames from "classnames";
+import { Project, projects } from "../../../state-defaults/projects";
 
 interface AboutCheckDepositProps {
   projects: { id: string; name: string }[];
@@ -7,6 +8,44 @@ interface AboutCheckDepositProps {
 }
 
 export function AboutCheckDeposit(props: AboutCheckDepositProps) {
+  const projectSelectorRef = useRef<HTMLDivElement>(null);
+  const [projectSelectorOpened, setProjectSelectorOpened] = useState(false);
+  const [project, setProject] = useState<Project | null>(projects[0]);
+
+  useEffect(() => {
+    if (!projectSelectorOpened) {
+      return;
+    }
+
+    const handleDocumentClick = (ev: MouseEvent) => {
+      if (
+        projectSelectorRef.current &&
+        ev.target instanceof Element &&
+        !projectSelectorRef.current.contains(ev.target)
+      ) {
+        setProjectSelectorOpened(false);
+      }
+    };
+
+    document.addEventListener("click", handleDocumentClick);
+
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, [projectSelectorOpened]);
+
+  const handleProjectOptionClick = (
+    ev: React.MouseEvent<HTMLLIElement, MouseEvent>
+  ) => {
+    const projectId = ev.currentTarget.getAttribute("data-project-id");
+    if (projectId !== null) {
+      const nextProject = projects.find(({ id }) => id === projectId);
+      setProject(nextProject || null);
+    }
+
+    setProjectSelectorOpened(false);
+  };
+
   return (
     <div className="main-content__deposit about-deposit">
       <div className="about-deposit__generation">
@@ -17,13 +56,19 @@ export function AboutCheckDeposit(props: AboutCheckDepositProps) {
         <div
           className="about-deposit__generation-select about-deposit__generation-selected--not-reverse about-deposit__generation-selected--white"
           data-select-wrapper=""
+          ref={projectSelectorRef}
         >
           <div
-            className="about-deposit__generation-selected"
+            className={classNames("about-deposit__generation-selected", {
+              active: projectSelectorOpened,
+            })}
             data-select-arrow=""
+            onClick={() => setProjectSelectorOpened(!projectSelectorOpened)}
           >
             <div className="about-deposit__generation-info">
-              <h5 className="about-deposit__generation-title">Выбор проекта</h5>
+              <h5 className="about-deposit__generation-title">
+                {project && project.name}
+              </h5>
             </div>
 
             <div className="about-deposit__generation-currency">
@@ -35,16 +80,25 @@ export function AboutCheckDeposit(props: AboutCheckDepositProps) {
             </div>
           </div>
 
-          <ul className="about-deposit__generation-list" data-select-list="">
-            <li className="about-deposit__generation-item" data-select-item="">
-              Выбор проекта
-            </li>
-            <li className="about-deposit__generation-item" data-select-item="">
-              Выбор проекта
-            </li>
-            <li className="about-deposit__generation-item" data-select-item="">
-              Выбор проекта
-            </li>
+          <ul
+            className={classNames("about-deposit__generation-list", {
+              active: projectSelectorOpened,
+            })}
+            data-select-list=""
+          >
+            {projects.map((project) => {
+              return (
+                <li
+                  className="about-deposit__generation-item"
+                  data-select-item=""
+                  data-project-id={project.id}
+                  key={project.id}
+                  onClick={handleProjectOptionClick}
+                >
+                  {project.name}
+                </li>
+              );
+            })}
           </ul>
         </div>
 
