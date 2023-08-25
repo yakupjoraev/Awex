0276c479629060cd40cca1b04858006ef92f6dda
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-08-25 09:17:40 +0300

    feat: bind to the backend Edit project page

---
+++ front-react/src/app/pages/EditProjectPage/EditProjectFooter/index.tsx
@@ -0,0 +1,9 @@
+export function EditProjectFooter() {
+  return (
+    <div className="my-projects__item-btns">
+      <button type="submit" className="my-projects__btn second-btn">
+        Сохранить данные
+      </button>
+    </div>
+  );
+}
---
+++ front-react/src/app/pages/EditProjectPage/EditProjectHeader/index.tsx
@@ -0,0 +1,62 @@
+import { Project } from "@awex-api";
+
+interface EditProjectHeaderProps {
+  project?: Project;
+}
+
+export function EditProjectHeader(props: EditProjectHeaderProps) {
+  const { project } = props;
+
+  let urlWeb = "...";
+  let feePayee = "...";
+  let currency = "...";
+
+  if (project) {
+    if (project.urlWeb) {
+      urlWeb = project.urlWeb;
+    }
+    feePayee = project.feePayee ? "мерчант" : "клиент";
+  }
+
+  return (
+    <div className="my-projects__item">
+      <div className="my-projects__item-info">
+        <h3 className="my-projects__item-title main-title">
+          {project ? project.name : "..."}
+          <img
+            className="my-projects__item-icon"
+            src="/img/icons/pen.svg"
+            alt="pen"
+          />
+        </h3>
+        <a
+          href={project ? project.urlWeb : "#"}
+          className="my-projects__item-address"
+          rel="noopener"
+          target="_blank"
+        >
+          {urlWeb}
+        </a>
+      </div>
+
+      <div className="my-projects__item-convertion">
+        <div className="my-projects__item-for">
+          <div className="my-projects__item-text">конвертация в:</div>
+          <div className="my-projects__item-currency">
+            <img
+              className="my-projects__item-pic"
+              src={"/img/actives/" + "actives-1.png"}
+              alt=""
+            />
+            <span className="my-projects__item-curr">{currency}</span>
+          </div>
+        </div>
+
+        <div className="my-projects__item-to">
+          <div className="my-projects__item-text">комиссию платит:</div>
+          <div className="my-projects__item-client">{feePayee}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
