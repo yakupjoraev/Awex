5e848b30d75c7e962a1155332474034486493a98
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-09-05 23:32:09 +0300

    feat: use data to render actives

--- front-react/src/app/pages/HomePage/MyActives/index.tsx
+++ front-react/src/app/pages/HomePage/MyActives/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { actives } from "../../../../data/actives";
+import { MyActivesChecks } from "@components/MyActivesChecks";
+import { MyActivesCheck } from "@components/MyActivesCheck";
+import { toast } from "react-hot-toast";
+
+const VISIBLE_ACTIVES = actives.slice(0, 4);
 
 export function MyActives() {
+  const handleNotImplemented = () => {
+    toast("NOT IMPLEMENTED!");
+  };
+
   return (
     <div className="my-actives">
       <div className="my-actives__header">
@@ -18,213 +28,20 @@ export function MyActives() {
       </div>
 
       <div className="my-actives__checks-container">
-        <ul className="my-actives__checks">
-          <li className="my-actives__check">
-            <div className="my-actives__check-info">
-              <div className="my-actives__check-currency">
-                <img
-                  className="my-actives__check-pic"
-                  src="/img/actives/actives-1.png"
-                  alt=""
-                />
-
-                <div className="my-actives__check-name">
-                  <h4 className="my-actives__check-title">RUB</h4>
-
-                  <p className="my-actives__check-detail">Russian Rubles</p>
-                </div>
-              </div>
-
-              <div className="my-actives__check-sums">
-                <h4 className="my-actives__check-title">1.789.567.57</h4>
-
-                <p className="my-actives__check-detail">1.789.567.57</p>
-              </div>
-            </div>
-
-            <div className="my-actives__check-operations">
-              <div className="my-actives__check-operation">
-                <img
-                  className="my-actives__check-operation-img"
-                  src="/img/icons/conclusion.svg"
-                  alt="Вывод"
-                />
-
-                <p className="my-actives__check-operation-descr">Вывод</p>
-              </div>
-            </div>
-          </li>
-
-          <li className="my-actives__check">
-            <div className="my-actives__check-info">
-              <div className="my-actives__check-currency">
-                <img
-                  className="my-actives__check-pic"
-                  src="/img/actives/actives-2.png"
-                  alt=""
-                />
-
-                <div className="my-actives__check-name">
-                  <h4 className="my-actives__check-title">Bitcoin</h4>
-
-                  <p className="my-actives__check-detail">BTC</p>
-                </div>
-              </div>
-
-              <div className="my-actives__check-sums">
-                <h4 className="my-actives__check-title">1.789.567.57</h4>
-
-                <p className="my-actives__check-detail">1.789.567.57</p>
-              </div>
-            </div>
-
-            <div className="my-actives__check-operations">
-              <div className="my-actives__check-operation">
-                <img
-                  className="my-actives__check-operation-img"
-                  src="/img/icons/conclusion.svg"
-                  alt="Вывод"
-                />
-
-                <p className="my-actives__check-operation-descr">Вывод</p>
-              </div>
-
-              <div className="my-actives__check-operation">
-                <img
-                  className="my-actives__check-operation-img"
-                  src="/img/icons/sell.svg"
-                  alt="Продать"
-                />
-
-                <p className="my-actives__check-operation-descr">Продать</p>
-              </div>
-
-              <div className="my-actives__check-operation">
-                <img
-                  className="my-actives__check-operation-img"
-                  src="/img/icons/swap.svg"
-                  alt="SWAP"
-                />
-
-                <p className="my-actives__check-operation-descr">SWAP</p>
-              </div>
-            </div>
-          </li>
-
-          <li className="my-actives__check">
-            <div className="my-actives__check-info">
-              <div className="my-actives__check-currency">
-                <img
-                  className="my-actives__check-pic"
-                  src="/img/actives/actives-3.png"
-                  alt=""
-                />
-
-                <div className="my-actives__check-name">
-                  <h4 className="my-actives__check-title">
-                    Green Metaverse Token
-                  </h4>
-
-                  <p className="my-actives__check-detail">PEOPLE</p>
-                </div>
-              </div>
-
-              <div className="my-actives__check-sums">
-                <h4 className="my-actives__check-title">1.789.567.57</h4>
-
-                <p className="my-actives__check-detail">1.789.567.57</p>
-              </div>
-            </div>
-
-            <div className="my-actives__check-operations">
-              <div className="my-actives__check-operation">
-                <img
-                  className="my-actives__check-operation-img"
-                  src="/img/icons/conclusion.svg"
-                  alt="Вывод"
-                />
-
-                <p className="my-actives__check-operation-descr">Вывод</p>
-              </div>
-
-              <div className="my-actives__check-operation">
-                <img
-                  className="my-actives__check-operation-img"
-                  src="/img/icons/sell.svg"
-                  alt="Продать"
-                />
-
-                <p className="my-actives__check-operation-descr">Продать</p>
-              </div>
-
-              <div className="my-actives__check-operation">
-                <img
-                  className="my-actives__check-operation-img"
-                  src="/img/icons/swap.svg"
-                  alt="SWAP"
-                />
-
-                <p className="my-actives__check-operation-descr">SWAP</p>
-              </div>
-            </div>
-          </li>
-
-          <li className="my-actives__check">
-            <div className="my-actives__check-info">
-              <div className="my-actives__check-currency">
-                <img
-                  className="my-actives__check-pic"
-                  src="/img/actives/actives-4.png"
-                  alt=""
-                />
-
-                <div className="my-actives__check-name">
-                  <h4 className="my-actives__check-title">Etherium</h4>
-
-                  <p className="my-actives__check-detail">ETH</p>
-                </div>
-              </div>
-
-              <div className="my-actives__check-sums">
-                <h4 className="my-actives__check-title">1.789.567.57</h4>
-
-                <p className="my-actives__check-detail">1.789.567.57</p>
-              </div>
-            </div>
-
-            <div className="my-actives__check-operations">
-              <div className="my-actives__check-operation">
-                <img
-                  className="my-actives__check-operation-img"
-                  src="/img/icons/conclusion.svg"
-                  alt="Вывод"
-                />
-
-                <p className="my-actives__check-operation-descr">Вывод</p>
-              </div>
-
-              <div className="my-actives__check-operation">
-                <img
-                  className="my-actives__check-operation-img"
-                  src="/img/icons/sell.svg"
-                  alt="Продать"
-                />
-
-                <p className="my-actives__check-operation-descr">Продать</p>
-              </div>
-
-              <div className="my-actives__check-operation">
-                <img
-                  className="my-actives__check-operation-img"
-                  src="/img/icons/swap.svg"
-                  alt="SWAP"
-                />
-
-                <p className="my-actives__check-operation-descr">SWAP</p>
-              </div>
-            </div>
-          </li>
-        </ul>
+        <MyActivesChecks>
+          {VISIBLE_ACTIVES.map((active) => {
+            const { id: key, ...rest } = active;
+            return (
+              <MyActivesCheck
+                {...rest}
+                key={key}
+                onWithdraw={handleNotImplemented}
+                onSell={handleNotImplemented}
+                onSwap={handleNotImplemented}
+              />
+            );
+          })}
+        </MyActivesChecks>
       </div>
     </div>
   );
