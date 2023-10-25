f2f106e8056a46546271f593c47da8d32f3192b2
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-10-25 15:06:30 +0300

    feat: Not Found error message

---
+++ front-react/src/app/components/NotFoundErrorMessage/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { HOME_PAGE_PATH } from "../../constants/path-locations";
+
+export interface NotFoundErrorMessageProps {}
+
+export function NotFoundErrorMessage(props: NotFoundErrorMessageProps) {
+  return (
+    <section className="page-404">
+      <div className="page-404__inner">
+        <h1 className="page-404__title">404</h1>
+
+        <p className="page-404__descr">что-то пошло не так</p>
+
+        <Link className="page-404__link main-btn" to={HOME_PAGE_PATH}>
+          Вернуться на Главную
+        </Link>
+      </div>
+    </section>
+  );
+}
--- front-react/src/app/pages/NotFoundPage/index.tsx
+++ front-react/src/app/pages/NotFoundPage/index.tsx
@@ -1,22 +1,11 @@
 import { Helmet } from "react-helmet-async";
-import { Link } from "react-router-dom";
-import { HOME_PAGE_PATH } from "../../constants/path-locations";
+import { NotFoundErrorMessage } from "@components/NotFoundErrorMessage";
 
 export function NotFoundPage() {
   return (
     <div className="wrapper">
       <Helmet title="Страница не найдена" />
-      <section className="page-404">
-        <div className="page-404__inner">
-          <h1 className="page-404__title">404</h1>
-
-          <p className="page-404__descr">что-то пошло не так</p>
-
-          <Link className="page-404__link main-btn" to={HOME_PAGE_PATH}>
-            Вернуться на Главную
-          </Link>
-        </div>
-      </section>
+      <NotFoundErrorMessage/>
     </div>
   );
 }
