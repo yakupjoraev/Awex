27af882740e50fb49b552a274eb3e1559ad19f8a
magius1000 <magdaca@yandex.ru>
2023-12-29 16:08:55 +0300

    Added blocked form function

--- front-react/src/app/containers/BlockProfileModalContainer.tsx
+++ front-react/src/app/containers/BlockProfileModalContainer.tsx
@@ -1,7 +1,5 @@
-import { CommonService } from "@awex-api"
 import { BlockProfileModal } from "@components/BlockProfileModal"
-import { useEffect, useState } from "react"
-import { toast } from "react-hot-toast"
+import { useState } from "react"
 
 
 export interface BlockProfileModalContainerProps {
@@ -13,18 +11,11 @@ export interface BlockProfileModalContainerProps {
 export function BlockProfileModalContainer(props: BlockProfileModalContainerProps) {
   const [loading, setLoading] = useState(false)
 
-
-  const handleSubmitBlock = (opts: { problem: string }) => {
-    // setError("not implemented")
-  }
-
-
   return (
     <BlockProfileModal
       open={props.open}
       loading={loading}
       onClose={props.onClose}
-      onSubmitProblem={handleSubmitBlock}
     />
   )
 }
